21bcdfd44d2c1260335bd10ea12b6d0bf556e4af
PrAe <praepech.t@gmail.com>
2023-08-20 13:33:35 +0700

    mem_management

--- mem.c
+++ mem.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
 
 int *mal;
 int a=0;
@@ -10,8 +9,6 @@ int inc(int n)
 {
     int d = 10;
     mal = (int *)malloc(sizeof(int));
-    if(n==INFINITY)
-        return 1;
     printf("&d=%p mal=%p &mal=%p &a=%p &i=%p\n", &d, mal,&mal, &a, &i);
     inc(n+i);
     free(mal);
@@ -20,5 +17,9 @@ int inc(int n)
 
 int main()
 {
-    inc(a);
+    printf("%d", inc(a));
 }
+
+
+
+
